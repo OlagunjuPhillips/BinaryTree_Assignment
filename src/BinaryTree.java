ead29cc906540443a354c6a3fc5158baf0c62239
Andrea Edwards <aedwards@xula.edu>
2020-04-11 10:35:03 -0500

    Add TODOs for Phillips to consider.  Only fix if TODO explicitly has FIX THIS.

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -14,7 +14,16 @@ public class BinaryTree<Type> {
         root = null;
     }
 
-
+    // TODO FIX THIS.  Phillips, I know this is how this code was written but it's not correct.  That is, it works but it
+    //  should not have a parameter.  Why?  Because the object that calls this method, already knows its root Node.
+    //  The call should be
+    //      someBinaryTree.preorder();
+    //  Notice that it's the object (someBinaryTree) that should be output, not some other Node parameter.
+    //  If the left subtree of someBinaryTree wants to call preorder().  This is done like
+    //      someBinaryTree.left.preorder();
+    //  or maybe
+    //      someBinaryTree.getLeft().preorder();
+    //
     public void preorder(Node node){
         if(node != null){
             System.out.print(node.key +" ");
@@ -25,11 +34,14 @@ public class BinaryTree<Type> {
         }
     }
 
+    // TODO FIX THIS.  Phillips, this is called a "helper method".  It is only needed to setup preorder or as a convenience
+    //  to make it easier to use preorder().  Just by making the change to preorder() as I mention above, this
+    //  convenience method is no longer needed.
     public void printPreorder(){
         preorder(this.root);
     }
 
-
+    // TODO Phillips, see my comments at preorder().
     public void inorder(Node node){
         if(node != null){
             inorder(node.left);
@@ -40,10 +52,12 @@ public class BinaryTree<Type> {
         }
     }
 
+    // TODO Phillips, see my comments at printPreorder().
     public void printInorder(){
         inorder(this.root);
     }
 
+    // TODO Phillips, see my comments at preorder().
     public void postorder(Node node){
         if(node != null){
             postorder(node.left);
@@ -54,10 +68,12 @@ public class BinaryTree<Type> {
         }
     }
 
+    // TODO Phillips, see my comments at printPreorder().
     public void printPostorder(){
         postorder(this.root);
     }
 
+    // TODO Phillips, see my comments at preorder();
     public boolean isFull(Node node){
         if(node == null){
             return true;
@@ -71,6 +87,7 @@ public class BinaryTree<Type> {
         return false;
     }
 
+    // TODO Phillips, see my comments at preorder();
     public int countNumberOfNodes(Node node){
         if(node == null){
             return 0;
@@ -78,6 +95,12 @@ public class BinaryTree<Type> {
         return 1 + countNumberOfNodes(node.left) + countNumberOfNodes(node.right);
     }
 
+    // TODO Phillips, see my comments at preorder();
+    //  This method needs the index and the numberOfNodes, but not the Node parameter.
+    //  This is the type of function that can use a helper/convenience method so the user can call
+    //      someBinaryTree.isComplete()
+    //  and the only statement in BinaryTree.isComplete() is
+    //      return this.isComplete( 0, this.size() );
     public boolean isComplete(Node node, int index, int numberOfNodes){
         if(node == null){
             return true;
@@ -90,6 +113,11 @@ public class BinaryTree<Type> {
                 isComplete(node.right, 2 *(index + 2), numberOfNodes);
     }
 
+    // TODO Phillips, see my comments at preorder().
+    //   This code determines if a single node has an empty left or an empty right.
+    //   This method should be called isNotLeaf().
+    //   To determine degenerate, the binary tree must look like a line of nodes so if left child, then all left children,
+    //   or if right child, then all right children.
     public boolean isDegenerate(Node node){
         if(node.left == null){
             return true;
@@ -100,6 +128,8 @@ public class BinaryTree<Type> {
         return false;
     }
 
+    // TODO Phillips, see my comments at preorder().
+    //  So the depth is only about going left?  What if the tree is a degenerate tree skewed to the right?
     public int findDepth(Node node){
         int depth = 0;
         while(node != null){
@@ -109,6 +139,8 @@ public class BinaryTree<Type> {
         return depth;
     }
 
+    // TODO Phillips, see my comments at preorder().
+    //  A perfect tree has same depth in right and left subtrees and every non-leaf has left and right subtrees.
     public boolean checkIsPerfect(Node node, int depth, int level){
         if(root == null){
             return true;
@@ -125,11 +157,13 @@ public class BinaryTree<Type> {
         return checkIsPerfect(node.left, depth, level+1) && checkIsPerfect(node.right, depth, level+1);
     }
 
+    // TODO Phillips, see my comments at printPreorder().
     public boolean isPerfect(Node node){
         int height = findDepth(node);
         return checkIsPerfect(node, height, 0);
     }
 
+    // TODO Phillips, see my comments at preorder().
     public int height(Node node){
         if(node == null){
             return 0;
@@ -138,6 +172,7 @@ public class BinaryTree<Type> {
         return 1 + Math.max(height(node.left), height(node.right));
     }
 
+    // TODO Phillips, see my comments at preorder().
     public boolean isBalanced(Node node){
         int leftHeight = 0;
         int rightHeight = 0;
@@ -155,12 +190,15 @@ public class BinaryTree<Type> {
         return false;
     }
 
+    // TODO Phillips, see my comments at preorder().
     public void insertNode(Node node, Type key){
         Queue<Node> queue = new LinkedList<>();
         queue.add(node);
         ArrayList<Node> checked = new ArrayList<>();
 
-
+        // TODO Phillips, it's useful to write a comment when a block of code (like this while-loop) is long
+        //  and complicated.  Maybe something like
+        //
         while(!queue.isEmpty()){
             node = queue.peek();
             queue.remove();
@@ -169,14 +207,14 @@ public class BinaryTree<Type> {
                 checked.add(node);
                 if (node.left == null) {
                     node.left = new Node(key);
-                    break;
+                    break;  // Oh no!
                 } else {
                     queue.add(node.left);
                 }
 
                 if(node.right == null){
                     node.right = new Node(key);
-                    break;
+                    break;  // Oh no!
                 }else{
                     queue.add(node.right);
                 }
@@ -186,9 +224,14 @@ public class BinaryTree<Type> {
         }
     }
 
+    // TODO FIX THIS.
+    //  (1) create a node by randomly selecting a name from the names array.
+    //  (2) randomly select either HEAD or TAIL.
+    //  (3) if HEAD then attach if left==null otherwise move left, flip coin again, and repeat until find an empty slot.
+    //  (4) if TAIL then attach if right==null otherwise move right, flip coin again, and repeat until find an empty slot.
     public BinaryTree<String> createRandomTree(String [] names){
         Random rand = new Random();
-        String[] coin = {"head", "tail"};
+        String[] coin = {"head", "tail"};  // TODO This should be an "enumerated type".
         BinaryTree<String> randomTree = new BinaryTree<>();
         randomTree.root = new Node(names[0]);
 
--- src/BinaryTreeDriver.java
+++ src/BinaryTreeDriver.java
@@ -5,63 +5,59 @@ public class BinaryTreeDriver {
     public static void main(String[] args){
         System.out.println("=== My first Binary Tree Program ===");
 
-        System.out.println("=== TREE1 DATA ===");
-        System.out.println();
-        System.out.println("Creating tree1... Done.");
+        System.out.println("=== TREE1 DATA ===\n");
+
+        System.out.println("Creating tree1... ");
         BinaryTree<Integer> tree1 = new BinaryTree<>();
         tree1.root = new Node(1);
         tree1.root.left = new Node(2);
         tree1.root.right = new Node(3);
         tree1.root.left.left = new Node(4);
-        System.out.println("Printing tree1 using preorder traversal...");
+        System.out.println("Done.");
+
+        System.out.println("\nPrinting tree1 using preorder traversal...");
         tree1.printPreorder();
         System.out.println();
         System.out.println("Done.");
-        System.out.println();
 
-        System.out.println("Checking if tree1 is full...");
+        System.out.println("\nChecking if tree1 is full... ");
         if(tree1.isFull(tree1.root)){
-            System.out.println("tree1 is full");
+            System.out.println("It is full");
         }else{
-            System.out.println("tree1 is not full");
+            System.out.println("It is not full");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree1 is complete...");
+        System.out.println("\nChecking if tree1 is complete... ");
         if(tree1.isComplete(tree1.root, 0, tree1.countNumberOfNodes(tree1.root))){
-            System.out.println("tree1 is complete.");
+            System.out.println("It is complete.");
         }else{
-            System.out.println("tree1 is not complete.");
+            System.out.println("It is not complete.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree1 is degenerate...");
+        System.out.println("\nChecking if tree1 is degenerate... ");
         if(tree1.isDegenerate(tree1.root)){
-            System.out.println("tree1 is degenerate.");
+            System.out.println("It is degenerate.");
         }else{
-            System.out.println("tree1 is not degenerate.");
+            System.out.println("It is not degenerate.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree1 is perfect...");
+        System.out.println("\nChecking if tree1 is perfect... ");
         if(tree1.isPerfect(tree1.root)){
-            System.out.println("tree1 is perfect.");
+            System.out.println("It is perfect.");
         }else{
-            System.out.println("tree1 is not perfect.");
+            System.out.println("It is not perfect.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree1 is balanced...");
+        System.out.println("\nChecking if tree1 is balanced... ");
         if(tree1.isBalanced(tree1.root)){
-            System.out.println("tree1 is balanced.");
+            System.out.println("It is balanced.");
         }else{
-            System.out.println("tree1 is not balanced.");
+            System.out.println("It is not balanced.");
         }
-        System.out.println();
 
-        System.out.println("=== TREE2 DATA ===");
+        System.out.println("\n=== TREE2 DATA ===");
         System.out.println();
-        System.out.println("Creating tree2... Done.");
+        System.out.println("Creating tree2... ");
         BinaryTree<Integer> tree2 = new BinaryTree<>();
         tree2.root = new Node (1);
         tree2.root.left = new Node(2);
@@ -73,55 +69,51 @@ public class BinaryTreeDriver {
         tree2.root.left.left.left = new Node(8);
         tree2.root.left.left.right = new Node(9);
         tree2.root.left.right.left = new Node(10);
-        System.out.println("Printing tree2 using inorder traversal...");
+        System.out.println("Done.");
+
+        System.out.println("\nPrinting tree2 using inorder traversal... ");
         tree2.printInorder();
         System.out.println();
         System.out.println("Done.");
-        System.out.println();
 
-        System.out.println("Checking if tree2 is full...");
+        System.out.println("\nChecking if tree2 is full... ");
         if(tree2.isFull(tree2.root)){
-            System.out.println("tree2 is full");
+            System.out.println("It is full");
         }else{
-            System.out.println("tree2 is not full");
+            System.out.println("It is not full");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree2 is complete...");
+        System.out.println("\nChecking if tree2 is complete... ");
         if(tree2.isComplete(tree2.root, 0, tree2.countNumberOfNodes(tree2.root))){
-            System.out.println("tree2 is complete.");
+            System.out.println("It is complete.");
         }else{
-            System.out.println("tree2 is not complete.");
+            System.out.println("It is not complete.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree2 is degenerate...");
+        System.out.println("\nChecking if tree2 is degenerate... ");
         if(tree2.isDegenerate(tree2.root)){
-            System.out.println("tree2 is degenerate.");
+            System.out.println("It is degenerate.");
         }else{
-            System.out.println("tree2 is not degenerate.");
+            System.out.println("It is not degenerate.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree2 is perfect...");
+        System.out.println("\nChecking if tree2 is perfect... ");
         if(tree2.isPerfect(tree2.root)){
-            System.out.println("tree2 is perfect.");
+            System.out.println("It is perfect.");
         }else{
-            System.out.println("tree2 is not perfect.");
+            System.out.println("It is not perfect.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree2 is balanced...");
+        System.out.println("\nChecking if tree2 is balanced... ");
         if(tree2.isBalanced(tree2.root)){
-            System.out.println("tree2 is balanced.");
+            System.out.println("It is balanced.");
         }else{
-            System.out.println("tree2 is not balanced.");
+            System.out.println("It is not balanced.");
         }
-        System.out.println();
 
-        System.out.println("=== TREE3 DATA ===");
-        System.out.println();
-        System.out.println("Creating tree3... Done.");
+        System.out.println("\n=== TREE3 DATA ===");
+
+        System.out.println("\nCreating tree3... ");
         BinaryTree<String> tree3 = new BinaryTree<>();
 
         tree3.root = new Node("apple");
@@ -129,59 +121,54 @@ public class BinaryTreeDriver {
         tree3.root.right = new Node("cherry");
         tree3.root.left.left = new Node("dates");
         tree3.root.left.right = new Node("elderberry");
-        System.out.println("Printing tree3 using postorder traversal...");
-        tree3.printPostorder();
-        System.out.println();
         System.out.println("Done.");
-        System.out.println();
 
-        System.out.println("Checking if tree3 is full...");
+        System.out.println("\nPrinting tree3 using postorder traversal...");
+        tree3.printPostorder();
+        System.out.println("\nDone.");
+
+        System.out.println("\nChecking if tree3 is full... ");
         if(tree3.isFull(tree3.root)){
-            System.out.println("tree3 is full");
+            System.out.println("It is full");
         }else{
-            System.out.println("tree3 is not full");
+            System.out.println("It is not full");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree3 is complete...");
+        System.out.println("\nChecking if tree3 is complete... ");
         if(tree3.isComplete(tree3.root, 0, tree3.countNumberOfNodes(tree3.root))){
-            System.out.println("tree3 is complete.");
+            System.out.println("It is complete.");
         }else{
-            System.out.println("tree3 is not complete.");
+            System.out.println("It is not complete.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree3 is degenerate...");
+        System.out.println("\nChecking if tree3 is degenerate... ");
         if(tree3.isDegenerate(tree3.root)){
-            System.out.println("tree3 is degenerate.");
+            System.out.println("It is degenerate.");
         }else{
-            System.out.println("tree3 is not degenerate.");
+            System.out.println("It is not degenerate.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree3 is perfect...");
+        System.out.println("\nChecking if tree3 is perfect... ");
         if(tree3.isPerfect(tree3.root)){
-            System.out.println("tree3 is perfect.");
+            System.out.println("It is perfect.");
         }else{
-            System.out.println("tree3 is not perfect.");
+            System.out.println("It is not perfect.");
         }
-        System.out.println();
 
-        System.out.println("Checking if tree3 is balanced...");
+        System.out.println("\nChecking if tree3 is balanced... ");
         if(tree3.isBalanced(tree3.root)){
-            System.out.println("tree3 is balanced.");
+            System.out.println("It is balanced.");
         }else{
-            System.out.println("tree3 is not balanced.");
+            System.out.println("It is not balanced.");
         }
 
 
-        System.out.println("Creating random tree...");
+        System.out.println("\nCreating random tree... ");
         BinaryTree<String> tree4 = new BinaryTree<>();
         tree4 = tree4.createRandomTree(NAMES);
         System.out.println("Done.");
-        System.out.println();
 
-        System.out.println("Printing tree4 using postorder traversal...");
+        System.out.println("\nPrinting tree4 using postorder traversal... ");
         tree4.printPostorder();
     }
 }
